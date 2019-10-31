ea52df95e987d84789b18843a848922f0559bde9
KAILINYmq <2534992864@qq.com>
2019-10-31 20:08:12 +0800

    建立用户数据库模型以及数据库迁移

--- PyGame_project/PyGame_project/settings.py
+++ PyGame_project/PyGame_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'PyGame_project.urls'
+
+AUTH_USER_MODEL = 'users.User'
 
 TEMPLATES = [
     {
@@ -115,7 +118,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
--- PyGame_project/users/models.py
+++ PyGame_project/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+# 昵称	    手机号	邮箱	密码	    性别	头像     注册开发者	真实姓名 邮箱验证状态
+# nickname	mobile	email	password	gender	avatar  developer	username email_active
+
+class User(AbstractUser):
+    """
+    用户模型
+    """
+    GENDER_CHOICES = (
+        ('M', '男'),
+        ('F', '女'),
+    )
+    nickname = models.CharField(blank=True, null=True, max_length=20, verbose_name='昵称')
+    avatar = models.FileField(upload_to='avatar/', verbose_name='头像')
+    mobile = models.CharField(blank=True, null=True, max_length=13, verbose_name='手机号')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True, verbose_name='性别')
+    developer = models.BooleanField(default=False, verbose_name='是否为开发者')
+    email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+
+    class Meta:
+        db_table = "tb_users"
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
