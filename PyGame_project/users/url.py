e67eca5e99e18091cf1b4017e2fd3f12aec79014
KAILINYmq <2534992864@qq.com>
2019-11-04 12:53:28 +0800

    用户名重复接口|手机号重复接口|邮箱重复接口

--- PyGame_project/users/url.py
+++ PyGame_project/users/url.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     # 注册
     url(r'^users/$', views.UserView.as_view()),
+    # 判断用户名是否存在
+    url(r'^usernames/(?P<nickname>\w{5,20})/count/$', views.UsernameCountView.as_view()),
+    # 判断手机号是否存在
+    url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
+    # 判断邮箱是否存在
+    url(r'^email/(?P<email>\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14})/count/$', views.EmailCountView.as_view()),
     # 登陆
     url(r'^authorizations/$', views.UserAuthorizationView.as_view()),
 ]
--- PyGame_project/users/views.py
+++ PyGame_project/users/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework_jwt.views import ObtainJSONWebToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 from .serializers import CreateUserSerializer
+from .models import User
 
+class UsernameCountView(APIView):
+    """判断用户名是否已存在"""
+    def get(self, request, nickname):
+        # 用nickname去User模型中查询此用户名的数据
+        # count:查到数据了会返回条数，没有查询到则会返回0
+        count = User.objects.filter(nickname=nickname).count()
+        data = {
+            'nickname': nickname,
+            'count': count
+        }
+        # 响应
+        return Response(data)
+
+class MobileCountView(APIView):
+    """判断手机号是否已存在"""
+    def get(self, request, mobile):
+        # 用username去User模型中查询此用户名的数据
+        count = User.objects.filter(mobile=mobile).count()
+        data = {
+            'mobile': mobile,
+            'count': count
+        }
+        # 响应
+        return Response(data)
+
+class EmailCountView(APIView):
+    """判断邮箱是否已存在"""
+    def get(self, request, email):
+        # 用Email去User模型中查询此用户名的数据
+        count = User.objects.filter(email=email).count()
+        data = {
+            'email': email,
+            'count': count
+        }
+        # 响应
+        return Response(data)
 
 class UserView(CreateAPIView):
     """
